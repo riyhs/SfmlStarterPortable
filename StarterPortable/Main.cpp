efa7e8d1e0eb99a0bb15298efe0b65d5a959c35a
Riyaldi Hasan <riy.2923@gmail.com>
2022-12-22 18:58:33 +0700

    Add project files.

---
+++ StarterPortable/Main.cpp
@@ -0,0 +1,35 @@
+#include"SFML\System.hpp"
+#include"SFML\Graphics.hpp"
+#include"SFML\Window.hpp"
+#include"SFML\Audio.hpp"
+#include"SFML\Network.hpp"
+
+using namespace sf;
+
+int main() {
+	
+	RenderWindow window(VideoMode(800, 600), "OKE JALAN", Style::Default);
+
+	while (window.isOpen())
+	{
+		Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == Event::Closed)
+				window.close();
+		}
+
+		// Update
+
+		// Draw
+		window.clear();
+		
+		// Draw stuff
+		
+		// Finished Drawing
+		window.display();
+
+	}
+
+	return 0;
+}
